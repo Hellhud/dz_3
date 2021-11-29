1fe0d1cef0716dec2dc3ca87c28877c957f02a10
Hellhound <hardoneloves@gmail.com>
2021-11-29 18:14:55 +0300

    Добавьте файлы проекта.

---
+++ dz_3/dz_3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main(){
+	const size_t size = 50;
+	int a[size];
+	int i = 0;
+	cout << " vvedite ima faila" << endl;
+	const string b = ".txt";
+	const string k = "C:\\Users\\User\\Desktop\\fail\\";
+	string u, c;
+	cin >> u;
+	c = k + u + b;
+	cout << c;
+	ofstream f (c);
+	for (int  element : a) {
+		f << i << " ";
+		i++;
+		}
+	f.close();
+	return 0;
+}
